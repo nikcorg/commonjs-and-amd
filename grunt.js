8e6eea2114f2b992c32ccad9d62e0436015d5085
Niklas Lindgren <niklas@sc5.io>
2013-04-16 20:50:56 +0300

    Generate requires.js on-the-fly

--- grunt.js
+++ grunt.js
@@ -1,66 +1,133 @@
+/*global module:false*/
 module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-requirejs");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
+    grunt.registerTask("generate-requires", "Generate requires.js", function(conf) {
+        var config = grunt.config(conf);
+        var deps = config.deps;
+        var output = config.output;
+        var xlist = config.exclude;
+
+        var buffer = 'require(["' + deps.
+            filter(function (lib) {
+                return xlist.indexOf(lib) === -1;
+            }).
+            join('","') + '"]);';
+
+        grunt.file.write(output, buffer);
+    });
+
+    var loaders = {
+            almond: "src/components/almond/almond.js",
+            cajon: "src/components/cajon/cajon.js",
+            requirejs: "src/components/requirejs/require.js"
+        };
     var defaults = {
             amdloader: "src/components/requirejs/require.js",
-            application: {
-                appDir: "src/app",
-                baseUrl: "../app",
-                cjsTranslate: true,
-                dir: "build/app",
-                findNestedDependencies: true,
-                keepBuildDir: true,
-                mainConfigFile: "src/requirejs.config.js",
-                optimize: "none",
-                useStrict: true,
-                modules: [
-                    { name: "run" }
-                ],
-                paths: {
-                    "backbone": "empty:",
-                    "backbone.layoutmanager": "empty:",
-                    "jquery": "empty:",
-                    "q": "empty:",
-                    "underscore": "empty:"
-                }
+            requireConfig: "src/requirejs.config.js",
+            dirs: {
+                build: "build",
+                debug: "debug",
+                temp: "temp"
             },
-            components: {
-                baseUrl: "src/app",
-                mainConfigFile: "src/requirejs.config.js",
-                cjsTranslate: false,
-                findNestedDependencies: true,
-                include: ["../requires.js"],
-                keepBuildDir: true,
-                out: "temp/components.js"
+            build: {
+                amdloader: loaders.requirejs
+            },
+            debug: {
+                amdloader: loaders.cajon
             }
         };
+    var deps = Object.keys(require("./component.json").dependencies);
+    var emptyDeps = deps.
+        filter(function (key) {
+            return Object.keys(loaders).indexOf(key) === -1;
+        }).
+        reduce(function (map, key) {
+            map[key] = "empty:";
+            return map;
+        }, {});
     var config = {
             defaults: defaults,
+            loaders: loaders,
             requirejs: {
-                release: {
-                    options: "<config:defaults.application>"
+                application: {
+                    options: {
+                        appDir: "src/app",
+                        baseUrl: "../app",
+                        cjsTranslate: true,
+                        dir: defaults.dirs.build + "/app",
+                        findNestedDependencies: true,
+                        keepBuildDir: true,
+                        mainConfigFile: defaults.requireConfig,
+                        optimize: "none",
+                        useStrict: true,
+                        modules: [
+                            { name: "run" }
+                        ],
+                        paths: emptyDeps
+                    }
                 },
                 components: {
-                    options: "<config:defaults.components>"
+                    options: {
+                        baseUrl: "src/app",
+                        mainConfigFile: defaults.requireConfig,
+                        cjsTranslate: false,
+                        findNestedDependencies: true,
+                        include: ["../../temp/requires"],
+                        keepBuildDir: true,
+                        logLevel: 1,
+                        out: "temp/components.js"
+                    }
                 }
             },
             min: {
-                release: {
-                    src: ["<config:defaults.amdloader>", "temp/components.js"],
-                    dest: "build/amdloader.js",
+                build: {
+                    src: ["<%= defaults.build.amdloader %>", "temp/components.js"],
+                    dest: "<%= defaults.dirs.build %>/amdloader.js",
+                    logLevel: 1
+                },
+                debug: {
+                    src: ["<%= defaults.debug.amdloader %>", "temp/components.js"],
+                    dest: '<%= defaults.dirs.debug %>/amdloader.js',
                     logLevel: 1
                 }
             },
             copy: {
-                release: {
+                build: {
                     files: {
-                        "build/": "src/*.html"
+                        "<%= defaults.dirs.build %>/index.html": "src/*.html"
+                    }
+                },
+                debug: {
+                    files: {
+                        "<%= defaults.dirs.debug %>/index.html": "src/*.html",
+                        "<%= defaults.dirs.debug %>/app/": "src/app/**/*"
                     }
                 }
+            },
+            clean: {
+                all: ["temp", "build", "debug"]
+            },
+            requires: {
+                deps: ["q", "underscore", "backbone", "backbone.layoutmanager", "jquery"],
+                output: defaults.dirs.temp + "/requires.js",
+                exclude: Object.keys(loaders)
             }
         };
 
     grunt.initConfig(config);
-    grunt.registerTask("build", "requirejs:release requirejs:components min copy");
+
+    /* Build final front end */
+    grunt.registerTask(
+        "build",
+        "clean requirejs:application generate-requires:requires requirejs:components min:build copy:build"
+        );
+
+    /* Build debug front end */
+    grunt.registerTask(
+        "debug",
+        "clean generate-requires:requires requirejs:components min:debug copy:debug"
+        );
 };
