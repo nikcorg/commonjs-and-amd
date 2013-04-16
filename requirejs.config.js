77ae147a31e7ff6d1e685015634d6b6089f3fda0
Niklas Lindgren <niklas@sc5.io>
2013-04-16 16:35:30 +0300

    Sorted libraries in paths struct

--- requirejs.config.js
+++ requirejs.config.js
@@ -1,6 +1,23 @@
+/*global window:false, require:true*/
 var require = {
     paths: {
         "backbone": "../components/backbone/backbone",
-        "underscore": "../components/underscore/underscore"
+        "backbone.layoutmanager": "../components/layoutmanager/backbone.layoutmanager",
+        "jquery": "../components/jquery/jquery",
+        "underscore": "../components/underscore/underscore",
+        "q": "../components/q/q"
+    },
+    shim: {
+        "backbone": {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        "underscore": {
+            exports: "_"
+        },
+        "backbone.layoutmanager": {
+            deps: ["backbone"],
+            exports: "Backbone"
+        }
     }
 };
