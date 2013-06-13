a9a1f20c4e50531830ca99e3d9460ea8956a8b75
Niklas Lindgren <niklas@sc5.io>
2013-06-13 15:08:20 +0300

    handle the case of an empty collection

--- client/app/collections/base.js
+++ client/app/collections/base.js
@@ -2,6 +2,9 @@ var Backbone = require("backbone");
 
 var BaseCollection = module.exports = Backbone.Collection.extend({
     maxId: function () {
+        if (this.length === 0) {
+            return 0;
+        }
         return Math.max.apply(Math, this.pluck("id"));
     }
 });
